ac2737e178767af43d704c79381acd5936a86e9d
bankkeez <sethitikul@gmail.com>
2018-07-20 11:42:32 +0700

    link complete

--- appPanel.js
+++ appPanel.js
@@ -9,7 +9,8 @@ $(function () {
         }
     });
 
-    $('body').toggleClass('day-background');
+    // $('body').toggleClass('day-background');
 
-    $('#cb1').on('click', function () { $('body').toggleClass('day-background'); });
+    // $('#cb1').on('click', function () { $('body').toggleClass('day-background'); });
+
 })
